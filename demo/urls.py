a57fbb3f646c371a012c0034f9bce2dc1e0500fa
NilkanthKK <nilkanth.koffeekodes@gmail.com>
2025-02-26 16:02:22 +0530

    Removed sensitive data NEW ONE

--- demo/urls.py
+++ demo/urls.py
@@ -3,26 +3,26 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('xml-to-json/', xml_to_json),  
-    path('json-to-xml/', json_to_xml),
-    path('send-notification/', send_notification),
-    path('send-notification/', send_notificationn),
+    # path('xml-to-json/', xml_to_json),  
+    # path('json-to-xml/', json_to_xml),
+    # path('send-notification/', send_notification),
+    # path('send-notification/', send_notificationn),
 
 
-    # BBPS
-    path('biller-mdm-request/', biller_mdm_request),
-    path('bill-fetch-request/', bill_fetch_request),
-    path('bill-payment-request/', bill_payment_request),
-    path('complaint-registration-request/', complaint_registration_request),
-    path('complaint-tracking-request/', complaint_tracking_request),
-    path('transaction-status-request/', transaction_status_request),
-    path('deposit-enquiry-request/', deposit_enquiry_request),
-    path('bill-validation-request/', bill_validation_request),
+    # # BBPS
+    # path('biller-mdm-request/', biller_mdm_request),
+    # path('bill-fetch-request/', bill_fetch_request),
+    # path('bill-payment-request/', bill_payment_request),
+    # path('complaint-registration-request/', complaint_registration_request),
+    # path('complaint-tracking-request/', complaint_tracking_request),
+    # path('transaction-status-request/', transaction_status_request),
+    # path('deposit-enquiry-request/', deposit_enquiry_request),
+    # path('bill-validation-request/', bill_validation_request),
 
-    path('webhook-status/', get_webhook_status),
+    # path('webhook-status/', get_webhook_status),
 
-    path('fetch-bill/', fetch_bill_view),
-    path('transaction-status/', transaction_status_view),
+    # path('fetch-bill/', fetch_bill_view),
+    # path('transaction-status/', transaction_status_view),
 
     
 ]
